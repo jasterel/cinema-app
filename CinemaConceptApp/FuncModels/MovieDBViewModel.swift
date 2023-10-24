66c433fc66a2a64b61723ac043e5f3423e811587
jasterel <jasterelafis777@gmail.com>
2023-10-24 21:09:40 +0300

    v1.0.1 (README LOG)

---
+++ CinemaConceptApp/FuncModels/MovieDBViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDBViewModel.swift
+//  CinemaConceptApp
+//
+//  Created by Maxim on 10/24/23.
+//
+
+import SwiftUI
+
+struct MovieDBViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDBViewModel()
+}
---
+++ CinemaConceptApp/FuncModels/TrendingItem.swift
@@ -0,0 +1,18 @@
+//
+//  TrendingItem.swift
+//  CinemaConceptApp
+//
+//  Created by Maxim on 10/24/23.
+//
+
+import SwiftUI
+
+struct TrendingItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrendingItem()
+}
